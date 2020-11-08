ff3146ad0b2b2532ae660d41a386fbc7d2f49f8f
Felix Hartenbach <felix.hartenbach@gmx.de>
2020-11-08 14:08:41 +0100

    refactor landmark visualization

--- src/landmark_visualizer/landmark_visualizer.cpp
+++ src/landmark_visualizer/landmark_visualizer.cpp
@@ -2,16 +2,32 @@
 // Created by bandera on 20.03.16.
 //
 #include <tf2_ros/transform_broadcaster.h>
+
 #include "../StargazerConversionMethods.h"
 #include "../ros_utils.h"
 #include "LandmarkVisualizerParameters.h"
 #include "ceres/rotation.h"
 #include "ros/ros.h"
 #include "stargazer/StargazerConfig.h"
+#include "stargazer/DebugVisualizer.h"
 #include "visualization_msgs/MarkerArray.h"
 
 using namespace stargazer;
 using namespace stargazer_ros_tool;
+
+static std_msgs::ColorRGBA Color(const float r, const float g, const float b, const float a = 1.) {
+  std_msgs::ColorRGBA color;
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.a = a;
+  return color;
+}
+
+static std_msgs::ColorRGBA NONE = Color(0., 0., 0., 0.);
+static std_msgs::ColorRGBA WHITE = Color(1., 1., 1.);
+static std_msgs::ColorRGBA BLACK = Color(0., 0., 0.);
+static std_msgs::ColorRGBA YELLOW = Color(1., 1., 0.);
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "static_landmark_visualizer");
@@ -32,6 +48,15 @@ int main(int argc, char** argv) {
   visualization_msgs::MarkerArray lm_msg;
   std::vector<geometry_msgs::TransformStamped> transforms;
 
+  // middle of landmarks (origin is at a corner)
+  const double middle = stargazer::Landmark::kGridDistance *
+                        (stargazer::Landmark::kGridCount - 1) / 2.;
+  // approximate landmark size (circuit board)
+  const double board_size =
+      stargazer::Landmark::kGridDistance * (stargazer::Landmark::kGridCount);
+  // sphere size (diameter, based on grid distance so they don't touch each other)
+  const double led_diameter = stargazer::Landmark::kGridDistance / 3.;
+
   for (auto& el : landmarks) {
     stargazer::Landmark& lm = el.second;
     std::string frame_id = "lm" + std::to_string(lm.id);
@@ -43,43 +68,35 @@ int main(int argc, char** argv) {
     pose2tf(lm.pose, transform);
     transforms.push_back(transform);
 
+    // Marker message with common parameters
+    visualization_msgs::Marker common_marker;
+    common_marker.lifetime = ros::Duration();
+    common_marker.header.frame_id = frame_id;
+    common_marker.id = lm.id;
+    common_marker.action = visualization_msgs::Marker::ADD;
+    common_marker.pose.orientation.w = 1;
+
     // Landmark
-    visualization_msgs::Marker marker;
-    marker.lifetime = ros::Duration();
-    marker.header.frame_id = frame_id;
-    marker.id = lm.id;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.ns = "Landmarks";
-    marker.pose.position.x = stargazer::Landmark::kGridDistance *
-                             (stargazer::Landmark::kGridCount / 2. - 1. / 2.);
-    marker.pose.position.y = stargazer::Landmark::kGridDistance *
-                             (stargazer::Landmark::kGridCount / 2. - 1. / 2.);
-    marker.pose.position.z = -stargazer::Landmark::kGridDistance / 2.;
-    marker.pose.orientation.w = 1;
-    marker.type = visualization_msgs::Marker::CUBE;
-    marker.scale.x = stargazer::Landmark::kGridDistance * (stargazer::Landmark::kGridCount);
-    marker.scale.y = stargazer::Landmark::kGridDistance * (stargazer::Landmark::kGridCount);
-    marker.scale.z = 0.02;
-    marker.color.r = 1.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 1.0f;
-    marker.color.a = 1.0;
-    lm_msg.markers.push_back(marker);
+    visualization_msgs::Marker board_marker(common_marker);
+    board_marker.ns = "Boards";
+    board_marker.pose.position.x = middle;
+    board_marker.pose.position.y = middle;
+    board_marker.pose.position.z = -(board_size * 0.01 + led_diameter) * 0.5;
+    board_marker.type = visualization_msgs::Marker::CUBE;
+    board_marker.scale.x = board_size;
+    board_marker.scale.y = board_size;
+    board_marker.scale.z = board_size * 0.01;
+    board_marker.color = BLACK;
+    lm_msg.markers.push_back(board_marker);
 
     // LEDS
-    visualization_msgs::Marker led_marker;
-    led_marker.lifetime = ros::Duration();
-    led_marker.header.frame_id = frame_id;
-    led_marker.id = lm.id;
-    led_marker.action = visualization_msgs::Marker::ADD;
+    visualization_msgs::Marker led_marker(common_marker);
+    led_marker.ns = "LEDs";
     led_marker.type = visualization_msgs::Marker::SPHERE_LIST;
-    led_marker.scale.x = 0.08;
-    led_marker.scale.y = 0.08;
-    led_marker.scale.z = 0.08;
-    led_marker.color.r = 1.0f;
-    led_marker.color.g = 1.0f;
-    led_marker.color.b = 0.0f;
-    led_marker.color.a = 1.0;
+    led_marker.scale.x = led_diameter;
+    led_marker.scale.y = led_diameter;
+    led_marker.scale.z = led_diameter;
+    led_marker.color = YELLOW;
     for (auto& led : lm.points) {
       geometry_msgs::Point pt;
       pt.x = std::get<(int)POINT::X>(led);
@@ -90,23 +107,15 @@ int main(int argc, char** argv) {
     lm_msg.markers.push_back(led_marker);
 
     // Text
-    visualization_msgs::Marker text_marker;
-    text_marker.lifetime = ros::Duration();
-    text_marker.header.frame_id = frame_id;
-    text_marker.id = 1000 * lm.id;
-    text_marker.text = std::to_string(lm.id);
-    text_marker.action = visualization_msgs::Marker::ADD;
+    visualization_msgs::Marker text_marker(common_marker);
+    text_marker.ns = "IDs";
+    text_marker.text = stargazer::getHexIDstring(lm.id);
     text_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
     text_marker.scale.z = 0.1;
-    text_marker.color.r = 1.0f;
-    text_marker.color.g = 1.0f;
-    text_marker.color.b = 0.0f;
-    text_marker.color.a = 1.0;
-    text_marker.pose.position.x = stargazer::Landmark::kGridDistance *
-                                  (stargazer::Landmark::kGridCount / 2. - 1. / 2.);
-    text_marker.pose.position.y = stargazer::Landmark::kGridDistance *
-                                  (stargazer::Landmark::kGridCount / 2. - 1. / 2.);
-    text_marker.pose.position.z = -stargazer::Landmark::kGridDistance * 2.;
+    text_marker.color = YELLOW;
+    text_marker.pose.position.x = middle;
+    text_marker.pose.position.y = middle;
+    text_marker.pose.position.z = -board_size * 0.2;
     text_marker.pose.orientation.w = 1;
     lm_msg.markers.push_back(text_marker);
   }
